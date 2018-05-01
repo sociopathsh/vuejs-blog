33d3d8c9696d4362438cd45de706036cff85d2b6
Sherlock <sherlock@gmail.com>
2018-05-02 01:17:07 +0630

    Route Parameters

---
+++ src/components/singleBlog.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="single-blog">
+        <h1>{{ body.title }}</h1>
+        <article>{{ body.body }}</article>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                id: this.$route.params.id,
+                body: {}
+            }
+        },
+        created() {
+            this.$http.get('http://jsonplaceholder.typicode.com/posts/' + this.id).then(function (data) {
+               this.body = data.body
+            });
+        }
+    }
+</script>
+
+<style scoped>
+    #single-blog {
+        max-width: 960px;
+        margin: 0 auto;
+    }
+</style>
