35a5aa2a8fa76887777846bc48067e9f78f7ac27
Sherlock <sherlock@gmail.com>
2018-05-01 14:55:07 +0630

    Selectbox Binding

--- src/components/addBlog.vue
+++ src/components/addBlog.vue
@@ -16,6 +16,10 @@
                 <label>Africa</label>
                 <input type="checkbox" value="africa" v-model="blog.categories">
             </div>
+            <label>Author:</label>
+            <select v-model="blog.author">
+                <option v-for="author in authors">{{ author }}</option>
+            </select>
         </form>
         <div id="preview">
             <h3>Preview Blog</h3>
@@ -27,6 +31,7 @@
             <ul>
                 <li v-for="category in blog.categories">{{ category }}</li>
             </ul>
+            <p>Author: {{ blog.author }}</p>
         </div>
     </div>
 </template>
@@ -38,8 +43,10 @@
                 blog: {
                     title: '',
                     content: '',
-                    categories: []
-                }
+                    categories: [],
+                    author: ''
+                },
+                authors: ['Sherlock','Microft','John']
             }
         }
     }
